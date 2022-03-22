eee4b0cacc07cf01b6f183c6e93f631ef09953df
Nikita An <inventstar@rambler.ru>
2022-03-22 03:41:29 +0300

    C# Developer_Module1_Homework. Task 1 and 2 completed.

---
+++ C_SharpDev_MOD1_HW/C_SharpDev_MOD1_HW_Task1/Task_1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SharpDev_MOD1_HW
+{
+    internal class Task_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_SharpDev_MOD1_HW/C_SharpDev_MOD1_HW_Task2/Task_2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SharpDev_MOD1_HW
+{
+    internal class Task_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+            Console.ReadKey();
+        }
+    }
+}
