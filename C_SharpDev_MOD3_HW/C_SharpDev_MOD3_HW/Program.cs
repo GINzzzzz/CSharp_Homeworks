5cfea00e002bac8925035656fc667d709d30326b
Nikita An <inventstar@rambler.ru>
2023-01-14 13:12:09 +0300

    Revert "C# Develop_Module3_Homework. Task 1-5."
    This reverts commit 31f8e55c9127186f754aeced3d5f410adf648e4e.

--- C_SharpDev_MOD3_HW/C_SharpDev_MOD3_HW/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.WriteLine("Введите любое число: ");
-int n = int.Parse(Console.ReadLine());
-
-if (n % 2 == 0) Console.WriteLine("\nЧисло целое");
-else Console.WriteLine("\nЧисло нецелое");
