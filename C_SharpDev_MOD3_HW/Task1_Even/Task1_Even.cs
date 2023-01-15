e25b2986ca2915ae7b7043179ac175f77a794c30
Nikita An <inventstar@rambler.ru>
2023-01-15 03:18:14 +0300

    CSharpDev_MOD3_HW: Task_1, Task_2.

---
+++ C_SharpDev_MOD3_HW/Task1_Even/Task1_Even.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("\nЧисло четное.");
+}
+else Console.WriteLine("\nЧисло нечетное.");
---
+++ C_SharpDev_MOD3_HW/Task2_BlackJack/Task2_BlackJack.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите количество карт в руке: ");
+int cnt_hand = int.Parse(Console.ReadLine());
+int summa_hand = 0;
+
+for (int i = 0; i < cnt_hand; i++)
+{
+    Console.Write($"Введите номинал карты {i+1}: ");
+    var card = Console.ReadLine();
+
+    switch(card)
+    {
+        case "J":
+            summa_hand += 10;
+            break;
+        case "Q":
+            summa_hand += 10;
+            break;
+        case "K":
+            summa_hand += 10;
+            break;
+        case "A":
+            summa_hand += 10;
+            break;
+        default:
+            summa_hand += int.Parse(card);
+            break;
+    }
+}
+
+Console.WriteLine($"Сумма карт в руке: {summa_hand}");
